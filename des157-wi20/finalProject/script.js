38e83bcda40122efa43e4bd6fde96b86cd89970c
nylagershoni <35282629+nylagershoni@users.noreply.github.com>
2020-03-13 20:32:44 -0700

    Add files via upload

--- des157-wi20/finalProject/script.js
+++ des157-wi20/finalProject/script.js
@@ -14,11 +14,11 @@ window.addEventListener('load', function() {
     const previous = document.getElementById('prev');
 
     //upper left corner of slider
-let leftPosition = 0;
-//To keep track of each slide
-let counter = 0;
-//sets the width of the slider (also in CSS)
-slider.style.width = totalWidth;
+    let leftPosition = 0;
+    //To keep track of each slide
+    let counter = 0;
+    //sets the width of the slider (also in CSS)
+    slider.style.width = totalWidth;
 
 
 
@@ -47,37 +47,52 @@ previous.addEventListener('click', function(evt) {
         slider.style.left = leftPosition;
     }
 });
-
 });
 
-var thisButton = document.querySelector('.cube');
-var heartsDiv = document.getElementById('hearts');
-var textField = document.getElementById('amount');
-var defaultButtonText = document.querySelector('.default-state span');
-var defaultButton = document.querySelector('.default-state');
 
-thisButton.addEventListener('click', function(event) {
+    // listen for the submission of the form
+    document.getElementById("myform").addEventListener("submit", function(event){
+        // prevent the default behavior
+        event.preventDefault();
 
-  if (textField.value !=''){
+        // get all the text fields from the form
+        const formData = document.querySelectorAll("input[type=text]");
+        // create an array to hold values from the form
+        const words = [];
 
-  this.className = 'cube flip';
-  setTimeout(function(){
+        //loop through the fields and add the words to the array, one at a time.
+        for( let i=0; i<formData.length; i++){
+            words.push(formData[i].value);    
+        }
+        //You can see the array in the console, if you want...
+        console.log(words);
 
-    heartsDiv.innerHTML = "<img src= 'images/hearts.gif'>";
-    var cat = document.createElement('img');
-    cat.setAttribute('src', 'images/cat.png');
-    cat.setAttribute('class', 'cat');
-    document.querySelector('body').appendChild(cat);
-    cat.className += 'moveImg';
+        //run a function to put the words into the madlib... Function below...
+        //Pass the array of words the user typed into the function
+        makeMadLib(words);
 
-  }, 300);
+    });
+
+    function makeMadLib(theWords){
+        const madlib = `Thank You For Your Donation!`;
+
+        //Get the madlib container
+        const mlContainer = document.getElementById('madlib');
+
+        //Change the guts of the container to your madlib
+        mlContainer.innerHTML = `<p>${madlib}</p>`;
+
+        //change the class on the madlib container so you can see it.
+        mlContainer.setAttribute("class", "visible");
+
+    }
+
+    var heartsDiv = document.getElementById('hearts');
+
+    this.className = 'submit';
+    setTimeout(function(){
   
-} else {
-  defaultButtonText.innerHTML = 'Please Enter an Amount';
-  defaultButton.selectAttribute('style', 'background-color: rgba(205,0,4,1.00)');
-  setTimeout(function() {
-    defaultButtonText.innerHTML = 'donate';
-    defaultButton.removeAttribute('style');
-  },2000);
-}
-});
+      heartsDiv.innerHTML = "<img src= 'images/stars.gif'>";
+      var cat = document.createElement('img');
+  
+    }, 300);
